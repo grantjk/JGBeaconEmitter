836e4f764da8cb846109cd2c822d574aa4e3cb24
John Grant <johnkgrant@gmail.com>
2014-02-28 20:43:36 -0500

    Initial Commit

---
+++ BeaconEmitter/JGAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAAppDelegate.h
+//  BeaconEmitter
+//
+//  Created by John Grant on 2014-02-28.
+//  Copyright (c) 2014 John Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
